0680033b74ea5e7f9cb8e2a22fd2b75e4ebe96ff
Sriram-S <sriraam29@gmail.com>
2017-02-27 00:31:55 +0530

    Removed unused import

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,22 +1,10 @@
 package com.udacity.gradle.builditbigger;
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ProgressBar;
-
-import com.example.hp.myapplication.backend.myApi.MyApi;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.example.mylibrary.*;
-import java.io.IOException;
 
 
 public class MainActivityFragment extends Fragment {
