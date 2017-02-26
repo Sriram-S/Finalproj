594ae9d0093518531132efeaee5d62389def5a0d
Sriram-S <sriraam29@gmail.com>
2017-02-26 16:59:06 +0530

    Android Test Implemented

---
+++ app/src/androidTest/java/AsyncTest/AsyncTaskTest.java
@@ -0,0 +1,52 @@
+package AsyncTest;
+
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+
+import com.udacity.gradle.builditbigger.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by HP on 26-02-2017.
+ */
+
+@RunWith(value = AndroidJUnit4.class)
+
+public class AsyncTaskTest extends AndroidTestCase {
+
+    MainActivityFragment.EndpointsAsyncTask endpointsAsyncTask;
+    String res=null;
+
+
+    public AsyncTaskTest() {
+        super();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    @Test
+    public void checkasync() throws InterruptedException, ExecutionException, TimeoutException {
+        endpointsAsyncTask.execute();
+        res=endpointsAsyncTask.get(20, TimeUnit.SECONDS);
+        Log.v("sdf",res);
+        assertNotNull(res);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
